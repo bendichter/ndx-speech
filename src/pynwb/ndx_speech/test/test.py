f5ad7333053daeaf8e3210c1ac3ceaa48a6faa27
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-05-27 13:48:10 -0700

    add test

--- src/pynwb/ndx_speech/test/test.py
+++ src/pynwb/ndx_speech/test/test.py
@@ -1,5 +1,10 @@
 from pynwb.epoch import TimeIntervals
+from ndx_speech import Transcription
+
 import pandas as pd
 
-TimeIntervals.from_dataframe(pd.DataFrame(
-    {'start_time': [.1, 2.], 'stop_time': [.8, 2.3], 'label': ['hello', 'there']}), name='name')
+words = TimeIntervals.from_dataframe(pd.DataFrame(
+    {'start_time': [.1, 2.], 'stop_time': [.8, 2.3], 'label': ['hello', 'there']}), name='words')
+
+tt = Transcription(words=words)
+
